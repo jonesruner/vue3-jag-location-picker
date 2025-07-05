de100a0d8dba7b3554c7ef9327af73b5efb0c17d
Jone <1750053308@qq.com>
2025-07-05 08:47:13 +0800

    版本1.1.5

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import { resolve } from "path";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
